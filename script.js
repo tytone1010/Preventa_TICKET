77c11301c0516824bf81e97efc90f871c063db15
tytone1010 <tytone1010@outlook.com>
2026-03-06 10:03:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const params = new URLSearchParams(window.location.search)
 
 function getParam(nombre){
-  return params.get(nombre) || ""
+  return decodeURIComponent(params.get(nombre) || "")
 }
 
 document.getElementById("id").innerText = getParam("id")
 document.getElementById("fecha").innerText = getParam("fecha")
 document.getElementById("cliente").innerText = getParam("cliente")
 
+document.getElementById("detalle").innerHTML = getParam("detalle")
+
 document.getElementById("subtotal").innerText = getParam("subtotal")
 document.getElementById("isv").innerText = getParam("isv")
 document.getElementById("total").innerText = getParam("total")
-
-const detalle = decodeURIComponent(getParam("detalle"))
-
-document.getElementById("detalle").innerHTML = detalle
