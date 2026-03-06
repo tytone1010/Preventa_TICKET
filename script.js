2395bbefb964fd3d4e5779396f1767de9418e23e
tytone1010 <tytone1010@outlook.com>
2026-03-06 09:58:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
 const params = new URLSearchParams(window.location.search)
 
-const pedido = params.get("pedido")
+function getParam(nombre){
+  return decodeURIComponent(params.get(nombre) || "")
+}
 
-fetch("pedidos/pedido_" + pedido + ".json")
+document.getElementById("id").innerText = getParam("id")
+document.getElementById("fecha").innerText = getParam("fecha")
+document.getElementById("cliente").innerText = getParam("cliente")
 
-.then(res => res.json())
+document.getElementById("detalle").innerHTML = getParam("detalle")
 
-.then(data => {
-
-document.getElementById("id").innerText = data.id
-document.getElementById("fecha").innerText = data.fecha
-document.getElementById("cliente").innerText = data.cliente
-
-document.getElementById("subtotal").innerText = data.subtotal
-document.getElementById("isv").innerText = data.isv
-document.getElementById("total").innerText = data.total
-
-let filas=""
-
-data.productos.forEach(p => {
-
-filas += `
-<tr>
-<td class="center">${p.cant}</td>
-<td class="center">${p.unidad}</td>
-<td class="center">${p.vend}</td>
-<td class="right">${p.punit}</td>
-<td class="right">${p.sub}</td>
-</tr>
-`
-
-})
-
-document.getElementById("detalle").innerHTML = filas
-
-})
+document.getElementById("subtotal").innerText = getParam("subtotal")
+document.getElementById("isv").innerText = getParam("isv")
+document.getElementById("total").innerText = getParam("total")
