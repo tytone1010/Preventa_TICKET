e06cd03d533f6b235f586b1251745e19ded6546e
tytone1010 <tytone1010@outlook.com>
2026-03-06 10:17:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
 const params = new URLSearchParams(window.location.search);
-
-function param(nombre){
-  const v = params.get(nombre);
-  return v ? decodeURIComponent(v) : "";
-}
-
-document.getElementById("id").textContent = param("id");
-document.getElementById("fecha").textContent = param("fecha");
-document.getElementById("cliente").textContent = param("cliente");
-
-document.getElementById("subtotal").textContent = param("subtotal");
-document.getElementById("isv").textContent = param("isv");
-document.getElementById("total").textContent = param("total");
-
-const detalle = param("detalle");
-
-if(detalle){
-  document.getElementById("detalle").innerHTML = detalle;
-}
+const pedido = params.get("pedido");
