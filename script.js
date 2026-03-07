a9ea736862868d52d7307ecf45040f1adfe200fc
tytone1010 <tytone1010@outlook.com>
2026-03-06 23:52:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 const params = new URLSearchParams(window.location.search)
 
-const ticket = params.get("ticket")
+const pedido = params.get("pedido")
 
-if(ticket){
-
-const html = decodeURIComponent(ticket)
-
-document.getElementById("ticket").innerHTML = html
-
+if(!pedido){
+document.body.innerHTML="Pedido no especificado"
 }
+
+fetch("data/"+pedido+".json")
+.then(res=>res.json())
+.then(data=>{
+
+document.getElementById("info").innerHTML = `
+<b>ID:</b> ${data.id}<br>
+<b>Fecha:</b> ${data.fecha}<br>
+<b>Cliente:</b> ${data.cliente}
+`
+
+let html=""
+
+data.detalle.forEach(p=>{
+
+html+=`
+<tr>
+
+<td class="center">${p.cantidad}</td>
+
+<td class="center">${p.unidad}</td>
+
+<td class="center">${p.vend}</td>
+
+<td class="right">L ${p.precio}</td>
+
+<td class="right">L ${p.sub}</td>
+
+</tr>
+`
+
+})
+
+document.getElementById("detalle").innerHTML=html
+
+document.getElementById("subtotal").innerText="L "+data.subtotal
+document.getElementById("isv").innerText="L "+data.isv
+document.getElementById("total").innerText="L "+data.total
+
+})
